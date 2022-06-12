0a7f2c493ceafec052be418fe78cfd79576cd599
Dokmos <andre.atitude.dokmos@gmail.com>
2022-06-11 21:42:12 -0300

    Adicionando models e jsondecode

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:developer';
 
+import 'package:bolixo/ui/palpites.dart';
 import 'package:dio/dio.dart';
 
 import 'model/rankingModel.dart';
@@ -13,7 +15,8 @@ class ApiService {
   Future<List<User>?> getUsers() async {
     List<User> users = [];
     try {
-      response = await dio.get(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+      response =
+          await dio.get(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
       if (response.statusCode == 200) {
         print(response.data);
         //lista
@@ -38,23 +41,60 @@ class ApiService {
 
   Future<List<RankingModel>> getRanking() async {
     List<RankingModel> ranking = [];
-    ranking.add(RankingModel(1, "Andre", 10, 1));
-    ranking.add(RankingModel(2, "Caldas", 1, 0));
-    ranking.add(RankingModel(3, "PA", 0, 0));
-    ranking.add(RankingModel(4, "Silas", 6, 0));
-    ranking.add(RankingModel(5, "User", 5, 0));
+    String data = '[ {"idUser": 1, "name": "Andre", "score": 10, "flies": 1},      {"idUser": 2, "name": "Caldas", "score": 6, "flies": 0},      {"idUser": 3, "name": "User", "score": 5, "flies": 0},      {"idUser": 4, "name": "Silas", "score": 1, "flies": 0},      {"idUser": 5, "name": "PA", "score": 0, "flies": 0}    ]';
 
-    // try {
-    //   response = await dio.get(ApiConstants.baseUrl + ApiConstants.rankingEndpoint);
-    //   if (response.statusCode == 200) {
-    //     print(response.data);
-    //     ranking = (response.data as List).map((item) {
-    //       return RankingModel.fromJson(item);
-    //     }).toList();
-    //   }
-    // } catch (e) {
-    //   log(e.toString());
-    // }
+    try {
+      // response = await dio.get(ApiConstants.baseUrl + ApiConstants.rankingEndpoint);
+      // if (response.statusCode == 200) {
+        print(data);
+        Iterable l = json.decode(data);
+        ranking = List<RankingModel>.from(l.map((model)=> RankingModel.fromJson(model)));
+        // lista = (response.data as List).map((item) {
+        //   return RankingModel.fromJson(item);
+        // }).toList();
+      // }
+    } catch (e) {
+      log(e.toString());
+    }
     return ranking;
+  }
+
+  Future<List<PalpiteModel>> getPalpitesFromUser(int idUser) async {
+    response.data = [
+      {
+        "id": 1,
+        "idMatch": 10,
+        "idUser": 1,
+        "golsTeam1": 0,
+        "golsTeam2": 0,
+        "score": 5,
+        "datetime": "2022-06-01"
+      },
+      {
+        "id": 23,
+        "idMatch": 10,
+        "idUser": 1,
+        "golsTeam1": 0,
+        "golsTeam2": 0,
+        "score": 5,
+        "datetime": "2022-06-01"
+      }
+    ];
+
+    List<PalpiteModel> palpites = [];
+    try {
+      response =
+          await dio.get(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+      if (response.statusCode == 200) {
+        print(response.data);
+        //lista
+        var lista = (response.data as List).map((item) {
+          return userFromJson(item);
+        });
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return palpites;
   }
 }
--- lib/api/urls.dart
+++ lib/api/urls.dart
@@ -2,4 +2,12 @@ class ApiConstants {
   static String baseUrl = 'https://jsonplaceholder.typicode.com';
   static String usersEndpoint = '/users';
   static String rankingEndpoint = '/ranking';
+  static String palpitesFromUserEndpoint = '/palpites';
+  static String matchesFromPhase = '/matches';
+
+  static String savePalpites = '/palpites';
+  static String saveMatches = '/matches';
+  static String calcScore = '/calcScore';
+
+
 }
