9e8a70b13c4431b8ad4d8d367de19dff09ea3fd2
C1310587 Silas Souza Fernandes <c1310587@interno.bb.com.br>
2022-06-26 14:51:29 -0300

    alterando chamada para login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bolixo/ui/home.dart';
 import 'package:flutter/material.dart';
+import 'package:bolixo/ui/sign_up.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
       ),
       // home: const Home(title: appTitle)
-      home: const Home(title: appTitle),
+      home: SignUp(authFormType: AuthFormType.signIn),
     );
   }
 }
