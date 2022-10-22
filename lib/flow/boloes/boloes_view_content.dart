791623b88dd09933f0cc857da4697f27d07af9a5
André Belle Menezes <andre.atitude.dokmos@gmail.com>
2022-10-22 15:28:10 -0300

    lista de boloes do user

--- lib/flow/boloes/boloes_view_content.dart
+++ lib/flow/boloes/boloes_view_content.dart
@@ -2,13 +2,15 @@ import '../../api/model/bolao_model.dart';
 import '../../api/model/ranking_item_model.dart';
 
 class BoloesViewContent {
-  List<BolaoModel> boloes = [];
+  List<BolaoModel?> boloes = [];
   bool isLoading = true;
 
   BoloesViewContent();
 
-  BoloesViewContent.fromApiModel(List<RankingItemModel> boloes) {
-    boloes = boloes;
+  BoloesViewContent.fromApiModel(List<RankingItemModel> ranking) {
+    for (final e in ranking) {
+      boloes.add(e.bolao);
+    }
     isLoading = false;
   }
 }
